45fc5cd586e10c46406a1235622921f3fc221057
Craig Smith <craig.smith@customd.com>
2021-02-18 22:51:09 +1300

    chore: doc update

--- Readme.md
+++ Readme.md
@@ -7,8 +7,40 @@ A [Laravel Analytics](https://github.com/spatie/laravel-analytics) widget collec
 
 This repository contains the code for these widgets. While the code is open-source, it's important to remember that you'll need to purchase a license before using this addon in production. Licenses cost \$5 and can be purchased from the [Statamic Marketplace](https://statamic.com/addons/Phpsa/statamic-analytics).
 
+Add Google Analytics reporting to the control panel of your Statamic 3 site!
+
+### Reporting Widgets
+
+- Page Views & Visitors
+- Most Viewed Pages
+- Top Countries
+- Top Browsers
+- Top Referrers
+
+## Charts
+
+- Standard Table
+- Bar Chart
+- Pie Chart
+- Line Chart
+- Doughnut Chart
+
+### Tracking
+
+- Configure Google Analytics ID
+- Tracking settings including debug & user tracking
+- Add tracking to your theme with one line of code
+
 **[Installation / Documentation](https://statamic-plugins.cgs4k.nz/docs/3.x/analytics)**
 
 ## Security
 
 If you discover any security related issues, please email vxdhost@gmail.com instead of using the issue tracker.
+
+## ScreenShots
+
+<img src="https://github.com/phpsa/statamic-analytics/raw/master/example.png" />
+<img src="https://github.com/phpsa/statamic-analytics/raw/master/barchart.png" />
+<img src="https://github.com/phpsa/statamic-analytics/raw/master/doughtnutchart.png" />
+<img src="https://github.com/phpsa/statamic-analytics/raw/master/linechart.png" />
+<img src="https://github.com/phpsa/statamic-analytics/raw/master/piechart.png" />
--- resources/js/cp.js
+++ resources/js/cp.js
@@ -2,29 +2,32 @@ import { Bar } from "vue-chartjs";
 import { Line } from "vue-chartjs";
 import { Pie } from "vue-chartjs";
 import { Doughnut } from "vue-chartjs";
-import { Radar } from "vue-chartjs";
-import { PolarArea } from "vue-chartjs";
-import { Bubble } from "vue-chartjs";
-import { Scatter } from "vue-chartjs";
-// import the plugin core
-import "chartjs-plugin-colorschemes/src/plugins/plugin.colorschemes";
 
-// import a particular color scheme
+import "chartjs-plugin-colorschemes/src/plugins/plugin.colorschemes";
 import { Tableau20 } from "chartjs-plugin-colorschemes/src/colorschemes/colorschemes.tableau";
+
+const chartOptions = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    colorschemes: {
+      scheme: Tableau20,
+    },
+  },
+  legend: {
+    display: false,
+    position: "bottom",
+    maxHeight: 200,
+    fullSize: false,
+  },
+};
 
 const BarChart = {
   extends: Bar,
   props: ["chartdata", "options"],
   mounted() {
-    const options = {
-      responsive: true,
-      maintainAspectRatio: false,
-      plugins: {
-        colorschemes: {
-          scheme: Tableau20,
-        },
-      },
-    };
+    let options = chartOptions;
+    options.legend.display = false;
     this.renderChart(this.chartdata, options);
   },
 };
@@ -33,15 +36,8 @@ const LineChart = {
   extends: Line,
   props: ["chartdata", "options"],
   mounted() {
-    const options = {
-      responsive: true,
-      maintainAspectRatio: false,
-      plugins: {
-        colorschemes: {
-          scheme: Tableau20,
-        },
-      },
-    };
+    let options = chartOptions;
+    options.legend.display = false;
     this.renderChart(this.chartdata, options);
   },
 };
@@ -50,16 +46,9 @@ const PieChart = {
   extends: Pie,
   props: ["chartdata", "options"],
   mounted() {
-    const options = {
-      responsive: true,
-      maintainAspectRatio: false,
-      plugins: {
-        colorschemes: {
-          scheme: Tableau20,
-        },
-      },
-    };
-    this.renderChart(this.chartdata, options);
+    let options = chartOptions;
+    options.legend.display = false;
+    this.renderChart(this.chartdata, chartOptions);
   },
 };
 
@@ -67,84 +56,7 @@ const DoughnutChart = {
   extends: Doughnut,
   props: ["chartdata", "options"],
   mounted() {
-    const options = {
-      responsive: true,
-      maintainAspectRatio: false,
-      plugins: {
-        colorschemes: {
-          scheme: Tableau20,
-        },
-      },
-    };
-    this.renderChart(this.chartdata, options);
-  },
-};
-
-const RadarChart = {
-  extends: Radar,
-  props: ["chartdata", "options"],
-  mounted() {
-    const options = {
-      responsive: true,
-      maintainAspectRatio: false,
-      plugins: {
-        colorschemes: {
-          scheme: Tableau20,
-        },
-      },
-    };
-    this.renderChart(this.chartdata, options);
-  },
-};
-
-const PolarChart = {
-  extends: PolarArea,
-  props: ["chartdata", "options"],
-  mounted() {
-    const options = {
-      responsive: true,
-      maintainAspectRatio: false,
-      plugins: {
-        colorschemes: {
-          scheme: Tableau20,
-        },
-      },
-    };
-    this.renderChart(this.chartdata, options);
-  },
-};
-
-const BubbleChart = {
-  extends: Bubble,
-  props: ["chartdata", "options"],
-  mounted() {
-    const options = {
-      responsive: true,
-      maintainAspectRatio: false,
-      plugins: {
-        colorschemes: {
-          scheme: Tableau20,
-        },
-      },
-    };
-    this.renderChart(this.chartdata, options);
-  },
-};
-
-const ScatterChart = {
-  extends: Scatter,
-  props: ["chartdata", "options"],
-  mounted() {
-    const options = {
-      responsive: true,
-      maintainAspectRatio: false,
-      plugins: {
-        colorschemes: {
-          scheme: Tableau20,
-        },
-      },
-    };
-    this.renderChart(this.chartdata, options);
+    this.renderChart(this.chartdata, chartOptions);
   },
 };
 
@@ -152,7 +64,3 @@ Statamic.$components.register("bar-chart", BarChart);
 Statamic.$components.register("line-chart", LineChart);
 Statamic.$components.register("pie-chart", PieChart);
 Statamic.$components.register("doughnut-chart", DoughnutChart);
-Statamic.$components.register("radar-chart", RadarChart);
-Statamic.$components.register("polar-chart", PolarChart);
-Statamic.$components.register("bubble-chart", BubbleChart);
-Statamic.$components.register("scatter-chart", ScatterChart);
